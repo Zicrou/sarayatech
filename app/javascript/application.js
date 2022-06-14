ac50703e05b2bfe79e0772e5daa295b11085b95b
Zicrou <aazseck@gmail.com>
2022-06-14 20:11:50 +0000

    Get ready

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,5 @@ import * as bootstrap from "bootstrap"
 import * as uikit from "uikit"
 import "trix";
 import "@rails/actiontext"
-<<<<<<< HEAD
-import WOW from "wowjs"
-=======
 import "wowjs";
 import "owl.carousel";
->>>>>>> integrating
